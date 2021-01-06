e05cd0282ceb5cedbd602475d0b8846e922bb054
acmenes <acmenescompositions@gmail.com>
2021-01-06 16:24:07 -0500

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-$('#movie-form').submit(function () {
+const removeButton = document.querySelectorAll("button")
+const movieItem = document.querySelectorAll("li")
+
+$('#movie-form').submit(function (e) {
+    e.preventDefault();
     console.log("it works")
     let $inputs = $('#movie-form :input')
     console.log($inputs)
-    $('ul').append('<li>It worked</li>')
+    $('ul').append('<li class="list-group-item">It worked</li> <button type="button" class="btn btn-danger btn-sm">x</button')
+    // $('li').append('<button type="button" class="btn btn-danger btn-sm">x</button')
+})
+
+$('btn btn-danger ').submit(function (e) {
+    e.preventDefault();
+    console.log("it works")
 })
